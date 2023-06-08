7bfaaeb1e7323bf965f915b575efe573b68bfe4b
snakagomi <tanakagoro0319@yahoo.co.jp>
2023-06-08 22:06:18 +0900

    implements add_task

--- src/tasks.rs
+++ src/tasks.rs
@@ -1,6 +1,9 @@
 use chrono::{serde::ts_seconds, DateTime, Local, Utc};
 use serde::Deserialize;
 use serde::Serialize;
+use std::io::{Result, Seek, SeekFrom};
+use std::path::PathBuf;
+use std::fs::{File, OpenOptions};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct Task {
@@ -15,4 +18,33 @@ impl Task {
     let created_at: DateTime<Utc> = Utc::now();
     Task {text, created_at}
   }
+}
+
+pub fn add_task(journal_path: PathBuf, task:Task) -> Result<()> {
+  let mut file = OpenOptions::new()
+    .read(true)
+    .write(true)
+    .create(true)
+    .open(journal_path)?;
+
+  let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+    Ok(tasks) => tasks,
+    Err(e) if e.is_eof() => Vec::new(),
+    Err(e) => Err(e)?,
+  };
+
+  file.seek(SeekFrom::Start(0));
+
+  tasks.push(task);
+  serde_json::to_witer(file, &tasks)?;
+
+  Ok(())
+}
+
+pub fn complete_task(journal_path: PathBuf, task_position: usize) -> Result<()> {
+
+}
+
+pub fn list_tasks(journal_path:PathBuf) -> Result<()> {
+
 }
